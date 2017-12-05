11710f3c199647541e4b09874c8d60c61f6d5281
Larisasmiles <larisasmiles@gmail.com>
2017-12-05 14:42:58 -0700

    added begin html and images

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+// You'll create a trivia form with multiple choice or true/false options (your choice).
+// all question data will be stored in an array 
+// each array element will be a question object 
+
+  // var gameQuestion = {
+      // question: "", 
+      // choices: [], 
+      // answer: "",
+  // };
+  
+  // creating elements array and using jquery to append to page  
+  // use this process:http://du.bootcampcontent.com/denver-coding-bootcamp/10-23-2017-DENVER-Class-Repository-FSF/blob/master/Class-Content/04-jquery/01-Activities/04-jQueryDrinkList/Solved/jquery-drink-list-solved.html
+  
+  //using jquery to append all of the questions(container) to the game page
+
+// The player will have a limited amount of time to finish the quiz. 
+// create timer
+// end game
+
+
+// The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
+// grabbing users correct input and output to screen
+// write a function for above that checks using jquery for the class 
+
+
+// Don't let the player pick more than one answer per question.
+// Don't forget to include a countdown timer.
